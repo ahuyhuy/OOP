259fc9d0c594680af6520437fcc28225644bb5fc
Alex Huynh <alexhuynh@Alexs-MBP.modem>
2025-08-02 15:31:34 +0930

    fixed 1.5

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 int count_evens(int number) {
-    if (number < 2) {
+    if (number < 1) {
         return 0;
     }
     int result = 0;
-    for (int i = 2; i < number; i += 2) {
-        result++;
+    for (int i = 1; i < number; i++) {
+        if (i % 2 == 0) {
+            result++;
+        }
     }
     return result;
 }
--- main-1-5.cpp
+++ main-1-5.cpp
@@ -4,7 +4,7 @@ using namespace std;
 extern int count_evens(int number);
 
 int main() {
-    int number = 6;
+    int number = 0;
     cout << "number of evens between 1 and " << number << " is: " << count_evens(number) << endl;
     return 0;
 }
